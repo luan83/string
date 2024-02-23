adedc25ca8fcafc13d2c68ffb7745e3cdd16bf4f
luan83 <68071485+luan83@users.noreply.github.com>
2024-02-23 11:40:42 -0300

    Add files via upload

---
+++ js/string2.js
@@ -0,0 +1,5 @@
+let nome = 'luan'
+let idade = '39'
+let altura = '1.70'
+let Dayane = 'Dayane'
+console.log (`seu nome é ${nome} com ${idade} anos de idade, com ${altura} de altura, bem grande e tambem tem sua parceira ${Dayane} hihi`)
