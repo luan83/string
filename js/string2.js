483e72d5bc6a8d2dc4c474e44143ec3a2a6b3341
luan83 <68071485+luan83@users.noreply.github.com>
2024-02-23 11:51:29 -0300

    Add files via upload

--- js/string2.js
+++ js/string2.js
@@ -2,4 +2,7 @@ let nome = 'luan'
 let idade = '39'
 let altura = '1.70'
 let Dayane = 'Dayane'
+let peso = '76'
+let numero = '7'
 console.log (`seu nome é ${nome} com ${idade} anos de idade, com ${altura} de altura, bem grande e tambem tem sua parceira ${Dayane} hihi`)
+console.log (`${peso}kg de peso, esta com uma boa quantidade de massa,${altura} de altura, esta bem grandinho e se somar ambos fica ${peso + numero}`)
